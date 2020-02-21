a62f47bb2718b9fae076543d0ae8603b87374e35
Aleksei Morozov <aleksei.morozov@mera.com>
2020-02-21 10:17:15 +0300

    init: add towel sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    if (!matrix || !matrix.length) return [];
+
+    return matrix.map((array, index) => index % 2 === 0 ? array : array.reverse()).flat();
 }
